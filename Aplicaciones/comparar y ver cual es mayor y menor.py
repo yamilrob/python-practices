9c64aac013985551a53e06079e6f7700186c0c8a
Yamil Rodriguez <yamildesalazar@gmail.com>
2023-04-21 12:21:27 -0500

    Modificaciones de nombre de archivos

---
+++ Aplicaciones/comparar y ver cual es mayor y menor.py
@@ -0,0 +1,9 @@
+#Escribir un programa que lea dos números enteros y los compare para determinar cuál es el mayor y cuál es el menor.
+
+num_1= int(input("Ingrese un número: "))
+num_2= int(input("Ingrese un número: "))
+
+if num_1 > num_2:
+    print("Este número es mayor")
+if num_1 < num_2:
+    print("Este número es menor")
