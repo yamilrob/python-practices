784b434401e76c5a50ba82acbc158de79670aea0
Yamil Rodriguez <yamildesalazar@gmail.com>
2023-04-22 15:03:59 -0500

    Se modifico el nombe

--- Aplicaciones/Orden ascendete.py
+++ Aplicaciones/Orden ascendete.py
@@ -1,6 +1,10 @@
 #Imprime los números del 1 al 10 en orden ascendente.
 
-for i in range(10, 0, -1):
-    print(i)
+# for i in range(10, 0, -1):
+#     print(i)
 
+
+for i in range (1,9):
+    x = i * 3
+    print(i)
 
