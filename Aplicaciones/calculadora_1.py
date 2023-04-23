f8c3c4a76751f960b621f735a60302aff4679238
Yamil Rodriguez <yamildesalazar@gmail.com>
2023-04-22 22:29:17 -0500

    se modifivco el nombre

---
+++ Aplicaciones/calculadora_1.py
@@ -0,0 +1,11 @@
+mensaje = """Bienvenidos a la Calculadora =)
+Operaciones permitidad son: sum, resta, mult, div.
+Para salir escriba Salir."""
+print(mensaje)
+
+
+num = input("Ingrese número: ")
+opera = input("Ingrese la opración que quire realizar: ")
+num2 = input("Ingrese otro número: ")
+
+print(num, opera, num2)
